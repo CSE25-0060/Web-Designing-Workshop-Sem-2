9d622e3a550ae8893c2ddc10f3586a968a71ae97
Sanskriti <sanskriti.1837@gmail.com>
2026-02-26 16:03:02 +0530

    New java files

---
+++ index.js
@@ -0,0 +1,41 @@
+//Introduction of variable
+/*{
+    var name="Sanskriti";
+    var name="Swabhiman";
+    console.log(name);
+}
+console.log(name);*/
+
+/*{
+    let a=10;
+    a="Vipin";
+    console.log(a);
+}
+console.log(a);*/
+
+/*{
+    const name="Sanskriti";
+    console.log(name);
+}
+console.log(name);*/
+
+//Datatypes
+//Primitive 
+let name="Monika";
+console.log(typeof name);
+
+let a=34;
+console.log(typeof a);
+
+let isstudent=true;
+console.log(typeof isstudent);
+
+let car=null;
+console.log(typeof car);
+
+let score;
+console.log(typeof score);
+
+
+
+
