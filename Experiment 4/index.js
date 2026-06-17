b43f07fcc2247b82777254f23c809855df9e5f72
CSE25-0060 <sanskriti.1837@gmail.com>
2026-06-17 23:49:10 +0530

    Experiment 4

--- Experiment 4/index.js
+++ Experiment 4/index.js
@@ -1,31 +1,37 @@
-function calculateresult()
-{
-    let n=document.getElementById("subject").value;
-    let total=0;
-    for(let i=1;i<=n;i++)
-    {
-        let marks=parseFloat(prompt("Enter marks for each subject"+i));
-        total+=marks;
-    }
-    let average=total/n;
-    let grade;
-    if(average>=90)
-        grade="A";
-    else if(average>80)
-        grade="B";
-    else if(average>=70)
-        grade="c";
-    else if(average>=60)
-        grade="d";
-    else
-        grade="F";
-
-    let result;    
-    if(average>=60)
-        result="pass";
-    else
-        result="fail";
-    document.getElementById("result").innerHTML="total marks: "+total+"<br>average marks: "+average+"<br>grade: "+grade+"<br>result: "+result;
-
-
-}
+function calculateResult(){ 
+let n = document.getElementById("subjects").value; 
+let total = 0; 
+for(let i=1;i<=n;i++){ 
+let marks = parseFloat(prompt("Enter marks for Subject " + i)); 
+total = total + marks; 
+} 
+let average = total / n; 
+let grade; 
+let result; 
+if(average >= 90){ 
+grade = "A+"; 
+} 
+else if(average >= 75){ 
+grade = "A"; 
+} 
+else if(average >= 60){ 
+grade = "B"; 
+} 
+else if(average >= 50){ 
+grade = "C"; 
+} 
+else{ 
+grade = "F"; 
+} 
+if(average >= 40){ 
+result = "PASS"; 
+} 
+else{ 
+result = "FAIL"; 
+} 
+document.getElementById("result").innerHTML =
+"Total Marks: " + total + "<br>" + 
+"Average Marks: " + average.toFixed(2) + "<br>" + 
+"Grade: " + grade + "<br>" + 
+"Result: " + result; 
+}  
